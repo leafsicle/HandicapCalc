ee54eccc712265b1bed3745cffb7972fa904d5ea
Mattclear <cookem529@gmail.com>
2022-09-05 07:17:26 -0400

    Set exit state for app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
-
-def handicapCalc (scores)
-  tidyScores = arrayManipulator(scores)
-  tidyScores.empty? 
-    (puts "it looks like you need to add more scores before you can get a hcap. Good luck")
-  avgScore = scoreAverage(tidyScores)
-  puts avgScore
-  return avgScore
+def app ()
+  puts 'If you\'re reading this, you\'re too early. Please come back later.'
+  # words = gets.chomp.split(" ")
+  # p words
+  # tidyScores = arrayManipulator(scores)
+  # tidyScores.empty? 
+  #   (puts "it looks like you need to add more scores before you can get a hcap. Good luck")
+  # avgScore = scoreAverage(tidyScores)
+  # puts avgScore
+  # return avgScore
+  exit()
 end
+app
