b3dc44840d37081b44b90699b39bf41f2a5561a0
MattCooke <cookem529@gmail.com>
2022-09-10 01:58:48 -0400

    Next happy path

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ end
 def get_name 
   puts "Welcome to the Golf Handicap Calculator\n"
   print "Hey friend, what should I call you?\n> "
-  return  gets.chomp
+  gets.chomp
 end
 def check_name (name)
   puts "Hello, #{name}, is that right?\n"
@@ -15,19 +15,20 @@ def check_name (name)
   confirmed = gets.chomp.to_i
   if confirmed == 1
     puts "Great, let's get started!\n"
+    user_stated_name = name
   elsif confirmed == 2
     puts "Oh, I'm sorry, I must have misheard you. What should I call you?\n"
     print "> "
-    user_stated_name = gets.chomp
+    check_name(gets.chomp)
   elsif confirmed = 3
     user_stated_name = "Friend"
     puts "hey friend, let's get started!\n"
   else
     clear_the_terminal
-    puts "Let's try that from the top, shall we?\n"
-    get_name()
+    puts "Let's try that from the top, shall we?\n\n"
+    app()
   end
-  userchosenName
+  user_stated_name
 end
 def prompt_user_for_scores (name)
   puts "Hi #{name}, Start by entering your scores for the last 20 rounds of golf.\n"
@@ -39,21 +40,18 @@ def get_scores
   # ask for scores
   # save scores to array
   # Course name with Score, slope, rating, and 9-hole or 18-hole course length
-  return gets.chomp
+  return "this is where the scores will go"
 end
 
 def app ()
+  total_hole_count = 0
   clear_the_terminal
   puts "If you're reading this, you're too early. Please come back later."
   user_stated_name = get_name
   userName = check_name (user_stated_name)
-    # ask if the user is uploading a file or entering text
   prompt_user_for_scores(userName)
-  user_scoresget_scores
+  user_scores = get_scores # get scores from user recursively until they stop entering scores || total_hole_count >= 360
  
-
-    # Score, slope, rating
-  #  if they are uploading a file, open the explorer  and let them select the file
   # tidyScores = arrayManipulator(scores)
   # tidyScores.empty? 
 
